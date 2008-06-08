468f367706394b0554f5532f9d5ab49967ae3ff8
Adrien BUSTANY <madcat@mymadcat.com>
2008-06-09 01:42:58 +0200

    Copyright stuff, for our friends at Novell Tidy the directory

--- lib/SignatureType.h
+++ lib/SignatureType.h
@@ -1,3 +1,14 @@
+/*
+* Copyright (C) 2007 Adrien Bustany <madcat@mymadcat.com>
+*
+* Licensed under the GNU General Public License Version 2
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 See MA 02111-1307, USA.
+*
+*/
+
 #ifndef SIGNATURETYPE_H
 #define SIGNATURETYPE_H
 
--- lib/constants.h
+++ lib/constants.h
@@ -1,2 +1,13 @@
+/*
+* Copyright (C) 2007 Adrien Bustany <madcat@mymadcat.com>
+*
+* Licensed under the GNU General Public License Version 2
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 See MA 02111-1307, USA.
+*
+*/
+
 #define PK_NAME				"org.freedesktop.PackageKit"
 #define PK_PATH				"/org/freedesktop/PackageKit"
