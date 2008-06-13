a06449f77b8b44fc41c8d612c612c9f514dbdeda
Adrien BUSTANY <madcat@mymadcat.com>
2008-06-13 23:41:36 +0200

    Add Enums to QPackageKit.h

--- lib/QPackageKit.h
+++ lib/QPackageKit.h
@@ -6,5 +6,13 @@
 
 // Enums
 #include "Exit.h"
+#include "Actions.h"
+#include "Error.h"
+#include "Exit.h"
+#include "Provides.h"
+#include "Restart.h"
+#include "Role.h"
+#include "SignatureType.h"
+#include "Status.h"
 
 #endif
